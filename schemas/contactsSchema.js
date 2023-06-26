e50cde99718c1b9ffdd382eb50e69e3cbd0f9d17
GregTerekhov <113180004+GregTerekhov@users.noreply.github.com>
2023-06-26 19:23:46 +0300

    small fix

--- schemas/contactsSchema.js
+++ schemas/contactsSchema.js
@@ -4,6 +4,7 @@ const contactsSchema = Joi.object({
   name: Joi.string().required(),
   email: Joi.string().email().lowercase().required(),
   phone: Joi.string()
+    .length(14)
     .pattern(
       /\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}/
     )
