75f849249716ada9cfe8b27c94344aba5ae078be
GregTerekhov <113180004+GregTerekhov@users.noreply.github.com>
2023-07-11 13:56:42 +0300

    small fix

--- controllers/auth/logout.js
+++ controllers/auth/logout.js
@@ -1,7 +1,7 @@
 const { User } = require("../../models");
 
 const logout = async (req, res) => {
-  const { _id } = req.User;
+  const { _id } = req.user;
   await User.findByIdAndUpdate(_id, { token: null });
   res.status(204);
   res.json();
