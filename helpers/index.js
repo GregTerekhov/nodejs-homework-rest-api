1053328e9eb405cedee87a57b4cf9148ee94f983
GregTerekhov <113180004+GregTerekhov@users.noreply.github.com>
2023-07-21 00:10:30 +0300

    The sixth task has done

--- helpers/index.js
+++ helpers/index.js
@@ -1,7 +1,9 @@
 const handleSchemaValidationErrors = require("./handleSchemaValidationErrors");
 const RequestError = require("./RequestError");
+const sendEmail = require("./sendEmail");
 
 module.exports = {
   handleSchemaValidationErrors,
   RequestError,
+  sendEmail,
 };
